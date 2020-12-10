9bd525748ec52b6cf4939ee623564cc869fb7927
Nezar <68679649+zar7364@users.noreply.github.com>
2020-12-10 21:12:31 +0700

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -5,31 +5,27 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
-# Penguin Prediction App
-
-This app predicts the **Palmer Penguin** species!
-
-Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+# Prediksi Jenis Pinguin - Data berasal dari [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) pada R oleh Allison Horst.
 """)
 
-st.sidebar.header('User Input Features')
+st.sidebar.header('Input Parameter')
 
 st.sidebar.markdown("""
 [Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
 """)
 
 # Collects user input features into dataframe
-uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+uploaded_file = st.sidebar.file_uploader("Upload dengan File CSV", type=["csv"])
 if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 else:
     def user_input_features():
-        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-        sex = st.sidebar.selectbox('Sex',('male','female'))
-        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
+        island = st.sidebar.selectbox('Pulau',('Biscoe','Dream','Torgersen'))
+        sex = st.sidebar.selectbox('Sex',('Jantan','Betina'))
+        bill_length_mm = st.sidebar.slider('Panjang Dawa (mm)', 32.1,59.6,43.9)
+        bill_depth_mm = st.sidebar.slider('Dalam Dawa (mm)', 13.1,21.5,17.2)
+        flipper_length_mm = st.sidebar.slider('Panjang Flipper (mm)', 172.0,231.0,201.0)
+        body_mass_g = st.sidebar.slider('Berat (g)', 2700.0,6300.0,4207.0)
         data = {'island': island,
                 'bill_length_mm': bill_length_mm,
                 'bill_depth_mm': bill_depth_mm,
@@ -76,5 +72,5 @@ st.subheader('Prediction')
 penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
 st.write(penguins_species[prediction])
 
-st.subheader('Prediction Probability')
+st.subheader('Prediction Probability in %')
 st.write(prediction_proba)
